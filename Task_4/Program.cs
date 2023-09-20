c215784f1e22ad80a085b7a562f81b0ac0389229
spam423 <zombe423@list.ru>
2023-09-21 00:23:14 +0300

    Task_4 live

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход 
+три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+int numberA, numberB, numberC;
+
+Console.WriteLine("first number");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("second number");
+numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("third number");
+numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
