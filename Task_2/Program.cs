18f076e50643e7c7fcf80e888ced7d475376c0a0
spam423 <zombe423@list.ru>
2023-09-20 23:34:05 +0300

    Task_2 job

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает два 
+числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+int numberA, numberB;
+
+Console.WriteLine("Введите первое число");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < numberB)
+{
+    Console.WriteLine(numberB);
+}
+
+else
+{
+    Console.WriteLine(numberA);
+}
