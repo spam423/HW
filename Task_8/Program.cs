d15d2d7b0f42d1d329f280ba72a8114469371750
spam423 <zombe423@list.ru>
2023-09-22 02:44:14 +0300

    Task_8 the end

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -3,3 +3,18 @@
 все чётные числа от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8*/
+
+using System.Collections;
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count < number)
+{
+    count = count + 1;
+    if (count % 2 == 0)
+    {
+        Console.Write(count);
+    }
+}
