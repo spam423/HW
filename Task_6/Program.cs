2adb910c89fdba4b90edd3ee638666a1541e30ef
spam423 <zombe423@list.ru>
2023-09-22 00:33:45 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+int number;
+int sum;
+
+Console.WriteLine("Enter number");
+number = Convert.ToInt32(Console.ReadLine());
+
+if
+
+( sum = number % 2 );
+
+Console.WriteLine( sum );
